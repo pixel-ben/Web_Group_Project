700d60fed90c33df50c81135b99728389e1b8248
40602751 <40602751@live.napier.ac.uk>
2025-03-29 17:08:04 +0000

    added index page

---
+++ javascript/index.js
@@ -0,0 +1,4 @@
+// Function to remove the fade overlay when clicking the "Continue" button
+function removeFade() {
+    document.getElementById('fade').classList.add('hidden'); // Adds 'hidden' class to fade out
+}
