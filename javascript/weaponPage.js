bf4e832e0860ffd329b03e5485f72c8ddd5c149b
pixel-ben <40658413@live.napier.ac.uk>
2025-04-20 06:53:42 +0100

    Update weaponPage.js
    added the js for the weapon descriptions 
    making the descriptions a constant and making it able to be changed

--- javascript/weaponPage.js
+++ javascript/weaponPage.js
@@ -4,11 +4,30 @@ function selectWeapon(weapon) {
     selectedWeapon = weapon;
     document.getElementById("chosen-weapon").innerText = `Selected Weapon: ${weapon}`;
 
+
+// Descriptions for each weapon
+const descriptions = {
+	Sword: "A strong reliable weapon seem's somewhat battle damaged and fragile though.\nDamage - High\nDefense - Normal\nspeed - Average\nRange - Average\nDurability - Weak",
+	dagger: "A very fast and reliable weapon seem's very new and un-used, best fitted for stealth attacks.\nDamage - Noraml\nDefense - Low\nspeed - Fast\nRange - Low\nDurability - High",
+	axe: "A heavy and powerful weapon only to be weilded by the strongest warriors best used for brute strength attacks.\nDamage - Extremly High\nDefense - Low\nspeed - Slow\nRange - High\nDurability - Average",
+	club: "A big club, looks like someone ripped a branch of a tree and put a skull on the end the branch seems to be rotting too.\nDamage - Low\nDefense - Low\nspeed - Average\nRange - Average\nDurability - weak"
+};
+
+   document.getElementById("weapon-description").innerText = descriptions[weapon];
+
+   // Update description
+   document.getElementById("weapon-description").innerText = descriptions[weapon];
+
+	
     // Remove previous selection
     document.querySelectorAll('.weapon').forEach(w => w.classList.remove('selected'));
 
     // Highlight selected weapon
-    event.target.closest('.weapon').classList.add('selected');
+	const weaponId = weapon.toLowerCase();
+	const weaponElement = document.getElementById(weaponId);
+	if (weaponElement) {
+		weaponElement.classList.add('selected');
+		}	
 }
 
 
